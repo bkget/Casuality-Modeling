5131e43d877ddcfeabed2ca5031f9b2c877e2691
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-29 09:23:28 +0300

    add a kdeplot to visualize features comparison

--- scripts/plots.py
+++ scripts/plots.py
@@ -73,3 +73,29 @@ class Plots:
         sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
         plt.title(f'Distribution of {column}', size=20, fontweight='bold')
         plt.show()
+
+
+    def feature_vs_target(self, df, x):    
+
+        fig,ax = plt.subplots(nrows = 10, ncols = 3, figsize = (12,24),dpi=80)
+        axes = ax.ravel()
+
+        for col,ax in zip(x.columns,axes):
+            # plots
+            sns.kdeplot(df[col], ax = ax, shade = True ,
+                        palette=["red", "green"],
+                        alpha = 0.5, linewidth = 1, ec = 'black',
+                        hue = df['diagnosis'], hue_order = ['M','B'],
+                        legend = False)
+
+            # plot setting
+            xlabel = ' '.join([value.capitalize() for value in str(col).split('_') ])
+            ax.axes.set_xlabel(xlabel,{'font':'serif','size':10, 'weight':'bold'}, alpha = 1)
+
+        plt.tight_layout(pad= 2,h_pad = 1, w_pad = 1)
+
+        fig.text(0.615,1, "\n       Benign",{'font':'serif','size':14, 'weight':'bold', 'color':"green"}, alpha = 1)
+        fig.text(0.735,1, '|',{'font':'serif','size':16, 'weight':'bold'})
+        fig.text(0.75,1, "  Malignant",{'font':'serif','size':14, 'weight':'bold','color':"red"}, alpha = 1)
+
+        fig.show()
