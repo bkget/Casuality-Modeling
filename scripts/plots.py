fdfde69a02c0374983d79b0c3c8cbd72e24aa85a
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-29 21:15:09 +0300

    update correlation heatmap plotting function

--- scripts/plots.py
+++ scripts/plots.py
@@ -39,12 +39,13 @@ class Plots:
         plt.show()
 
 
-    def heatmap(self, df, title='', annot=True): 
-        plt.figure(figsize=(12,6))
-        plt.title(title)
+    def heatmap(self, df: pd.DataFrame, title: str, cbar=False) -> None:
         correlation = df.corr()
-        sns.heatmap(correlation,square = True, linewidths = .5, cmap = "BuPu", annot=annot)
-        return
+        plt.figure(figsize=(20, 17))
+        sns.heatmap(correlation, annot=True, cmap='viridis', vmin=0,
+                    vmax=1, fmt='.2f', linewidths=.7, cbar=cbar)
+        plt.title(title, size=18, fontweight='bold')
+        plt.show()
          
 
     def plot_bar(self, column, title, xlabel, ylabel):
